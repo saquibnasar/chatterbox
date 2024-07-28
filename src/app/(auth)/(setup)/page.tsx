acd7c51109d089a7a5c18b6abff1dc557877b8f5
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-07-29 03:37:26 +0530

    form

--- src/app/(auth)/(setup)/page.tsx
+++ src/app/(auth)/(setup)/page.tsx
@@ -1,12 +1,20 @@
 import { initialProfile } from "@/lib/initial-profile";
 import db from "@/db/db";
-
+import { redirect } from "next/navigation";
+import InitialModal from "@/components/modals/initial-modal";
 export default async function page() {
   const profile = await initialProfile();
   const server = await db.server.findFirst({
     where: {
-      member: {},
+      member: {
+        some: {
+          profileId: profile.id,
+        },
+      },
     },
   });
-  return <>create a serverf</>;
+  if (server) {
+    return redirect(`/server/${server.id}`);
+  }
+  return <InitialModal />;
 }
