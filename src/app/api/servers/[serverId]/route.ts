d99a7cf6c319f4e6b68b2f6cfe1ee1b877bc3121
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-09-02 03:49:32 +0530

    route changes

--- src/app/api/servers/[serverId]/route.ts
+++ src/app/api/servers/[serverId]/route.ts
@@ -10,6 +10,9 @@ export async function PATCH(
     if (!profile) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
+    if (!params.serverId) {
+      return new NextResponse("Server ID Missing", { status: 400 });
+    }
   } catch (eror) {
     console.log("[SERVER_ID]", eror);
     return new NextResponse("Internal Error", { status: 500 });
