25fda4fd62339542db5cf692d015c372d2592f8c
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-09-11 20:50:51 +0530

    css to change user type

--- src/components/modals/MembersModal.tsx
+++ src/components/modals/MembersModal.tsx
@@ -7,14 +7,35 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { useModal } from "@/hooks/useModalStore";
-import { useOrigin } from "@/hooks/useOrigin";
 import { serverWithMembersWithProfile } from "../../../types";
 import { ScrollArea } from "../ui/scroll-area";
 import UserAvatar from "../ui/UserAvatar";
-
+import {
+  MoreVertical,
+  Shield,
+  ShieldAlert,
+  ShieldCheck,
+  ShieldQuestion,
+} from "lucide-react";
+import { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  ADMIN: <ShieldAlert className="h-4 w-4 text-rose-500" />,
+};
 export default function MembersModal() {
   const { isOpen, onClose, type, data, onOpen } = useModal();
-  const origin = useOrigin();
+  const [loadingId, setLoadingId] = useState("");
 
   const isModalOpen = isOpen && type === "members";
   const { server } = data as { server: serverWithMembersWithProfile };
@@ -38,7 +59,39 @@ export default function MembersModal() {
                     className="flex items-center gap-x-2 mb-6"
                   >
                     <UserAvatar src={member.profile.imageUrl} />
-                    <div className="flex flex-col gap-y-1"></div>
+                    <div className="flex flex-col gap-y-1">
+                      <div className="text-xs font-semibold flex items-center">
+                        {member.profile.name} {roleIconMap[member.role]}
+                      </div>
+                      <div className="text-xs text-zinc-500">
+                        {member.profile.email}
+                      </div>
+                    </div>
+                    {server.profileId !== member.profileId &&
+                      loadingId !== member.id && (
+                        <div className="ml-auto">
+                          <DropdownMenu>
+                            <DropdownMenuTrigger>
+                              <MoreVertical />
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent side="left">
+                              <DropdownMenuSub>
+                                <DropdownMenuSubTrigger className="flex items-center">
+                                  <ShieldQuestion className="w-4 h-4 mr-2" />
+                                  <span>role</span>
+                                </DropdownMenuSubTrigger>
+                                <DropdownMenuPortal>
+                                  <DropdownMenuSubContent>
+                                    <DropdownMenuItem>
+                                      <Shield className="h-4 w-4 mr-2" /> GUEST
+                                    </DropdownMenuItem>
+                                  </DropdownMenuSubContent>
+                                </DropdownMenuPortal>
+                              </DropdownMenuSub>
+                            </DropdownMenuContent>
+                          </DropdownMenu>
+                        </div>
+                      )}
                   </div>
                 ))}
               </ScrollArea>
