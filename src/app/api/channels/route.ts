21991a5bbad8d37e1550b47a6b8c354629f08ed8
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-09-18 09:33:31 +0530

    create channel

---
+++ src/app/api/channels/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from "next/server";
+import currentProfile from "@/lib/currentProfile";
+import db from "@/db/db";
+
+export async function POST(req: Request) {
+  try {
+    const profile = await currentProfile();
+    const { name, type } = await req.json();
+    const { searchParams } = new URL(req.url);
+
+    const serverId = searchParams.get("serverId");
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (name == -"general") {
+      return new NextResponse("Name cannot be 'general'", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: ["ADMIN", "MODERATOR"],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          create: {
+            profileId: profile.id,
+            name,
+            type,
+          },
+        },
+      },
+    });
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_POST]", error);
+    return new NextResponse("Internet Error", { status: 500 });
+  }
+}
--- src/components/modals/createChannelModal.tsx
+++ src/components/modals/createChannelModal.tsx
@@ -1,7 +1,8 @@
 import * as z from "zod";
 import axios from "axios";
+import qs from "query-string";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { useModal } from "@/hooks/useModalStore";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -16,6 +17,13 @@ import {
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 const ChannelTypes = z.enum(["TEXT", "AUDIO", "VIDEO"]);
 const formSchema = z.object({
@@ -27,11 +35,12 @@ const formSchema = z.object({
     .refine((name) => name !== "general", {
       message: "channel name cannot be 'general'",
     }),
-  type: ChannelTypes,
+  type: z.enum(["TEXT", "AUDIO", "VIDEO"]),
 });
 export default function CreateChannelModal() {
   const { isOpen, onClose, type } = useModal();
   const router = useRouter();
+  const params = useParams();
 
   const isModalOpen = isOpen && type === "createChannel";
 
@@ -39,7 +48,7 @@ export default function CreateChannelModal() {
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      type: "TEXT",
+      type: "Text",
     },
   });
 
@@ -47,8 +56,13 @@ export default function CreateChannelModal() {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      await axios.post("/api/servers", values);
-
+      const url = qs.stringifyUrl({
+        url: `/api/channels`,
+        query: {
+          serverId: params.serverId,
+        },
+      });
+      await axios.post(url, values);
       form.reset();
       router.refresh();
       onClose();
@@ -92,6 +106,44 @@ export default function CreateChannelModal() {
                             {...field}
                           />
                         </FormControl>
+                      </FormItem>
+                    );
+                  }}
+                />
+                <FormField
+                  control={form.control}
+                  name="type"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                          Channel Type
+                        </FormLabel>
+                        <Select
+                          disabled={isLoading}
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger className="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none">
+                              <SelectValue placeholder="select a channel type" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            {Object.values(["TEXT", "AUDIO", "VIDEO"]).map(
+                              (type) => (
+                                <SelectItem
+                                  key={type}
+                                  value={type}
+                                  defaultValue="TEXT"
+                                  className="captalize"
+                                >
+                                  {type.toLowerCase()}
+                                </SelectItem>
+                              )
+                            )}
+                          </SelectContent>
+                        </Select>
                       </FormItem>
                     );
                   }}
