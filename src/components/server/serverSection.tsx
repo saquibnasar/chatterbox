14710d93f5531b4da49305bcfd4bfb3cc8f4ea9b
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-09-23 16:04:56 +0530

    typo

--- src/components/server/serverSection.tsx
+++ src/components/server/serverSection.tsx
@@ -39,7 +39,7 @@ export default function ServerSection({
         )}
 
         {role === "ADMIN" && sectionType === "members" && (
-          <ActionTooltip label="Mange Members" side="top">
+          <ActionTooltip label="Manage Members" side="top">
             <button
               onClick={() => onOpen("members", { server })}
               className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
