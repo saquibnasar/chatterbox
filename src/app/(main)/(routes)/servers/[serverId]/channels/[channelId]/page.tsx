8927fcd10c23ea18b8af5a826b4f75ae31a6e24a
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-09-28 16:13:57 +0530

    sheet added

--- src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,3 +1,43 @@
-export default function ChannelIdPage() {
-  return <div>Channels id page</div>;
+import ChatHeader from "@/components/chat/chatHeader";
+import db from "@/db/db";
+import currentProfile from "@/lib/currentProfile";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+type ChannelIdPageProps = {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+};
+export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
+  const profile = await currentProfile();
+  if (!profile) {
+    return <RedirectToSignIn />;
+  }
+  const channel = await db.channel.findFirst({
+    where: {
+      id: params.channelId,
+    },
+  });
+  const member = await db.members.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+  if (!channel || !member) {
+    redirect("/");
+  }
+  return (
+    <>
+      <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+        <ChatHeader
+          name={channel.name}
+          serverId={channel.serverId}
+          type="channel"
+        />
+      </div>
+    </>
+  );
 }
---
+++ src/components/mobileToggle.tsx
@@ -0,0 +1,21 @@
+import { Menu } from "lucide-react";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import NavigationSidebar from "./navigation/navigationSidebar";
+
+export default function MobileToggle() {
+  return (
+    <>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="ghost" size="icon" className="md:hidden">
+            <Menu />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="p-0 flex gap-0">
+          <div className="w-[72px]">{/* <NavigationSidebar /> */}</div>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+}
