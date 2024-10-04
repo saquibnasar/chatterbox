b8ffa04c27086d38602f02d66b7b03c6f09780fc
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-10-04 23:41:02 +0530

    socket added

---
+++ page/api/socket/io.ts
@@ -0,0 +1,3 @@
+import { Server as Netserver } from "http";
+import { NextApiRequest } from "next";
+import { Server as serverId } from "socket.io";
--- src/components/chat/chatHeader.tsx
+++ src/components/chat/chatHeader.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react";
 import React from "react";
 import MobileToggle from "../mobileToggle";
+import UserAvatar from "../ui/UserAvatar";
 
 type ChatHeaderProps = {
   serverId: string;
@@ -23,6 +24,9 @@ export default function ChatHeader({
         <MobileToggle serverId={serverId} />
         {type === "channel" && (
           <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+        )}
+        {type === "conversation" && (
+          <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
         )}
         <p className="font-semibold text-md text-black dark:text-white">
           {name}
