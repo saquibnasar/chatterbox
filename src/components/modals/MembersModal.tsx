dfb6462d5f8b9dd49eb24c320cf660f3a1214d26
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-10-14 23:09:59 +0530

    test

--- src/components/modals/MembersModal.tsx
+++ src/components/modals/MembersModal.tsx
@@ -90,6 +90,7 @@ export default function MembersModal() {
       setLoadingId("");
     }
   };
+  const onCLickTEST = () => {};
   return (
     <>
       <Dialog open={isModalOpen} onOpenChange={onClose}>
