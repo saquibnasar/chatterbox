1cfdfe437af195a04d11081ad10c2bd9eb362f95
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-10-20 23:37:38 +0530

    added pdf upload

--- src/components/fileUpload.tsx
+++ src/components/fileUpload.tsx
@@ -5,7 +5,7 @@ import "@uploadthing/react/styles.css";
 // import { Uploader } from "@uploadthing/solid";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 // import type { OurFileRouter } from "@/server/uploadthing";
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 type FileUploadProps = {
   onChange: (url: string) => void;
@@ -26,6 +26,29 @@ export default function FileUpload({
         <button
           onClick={() => onChange("")}
           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  if (value && fileTYpe === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-0 shadow-sm"
           type="button"
         >
           <X className="h-4 w-4" />
