58657fd7f6ac058f0ef6c3867e29e26de0833409
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-10-25 20:42:02 +0530

    added usechatquery

--- src/components/chat/chatMessages.tsx
+++ src/components/chat/chatMessages.tsx
@@ -2,6 +2,7 @@
 
 import { Members } from "@prisma/client";
 import ChatWelcome from "./chatWelcome";
+import { useChatQuery } from "@/hooks/useChatQuery";
 
 interface ChatMessagesProps {
   name: string;
@@ -24,11 +25,13 @@ export default function ChatMessages({
   paramValue,
   type,
 }: ChatMessagesProps) {
+  const queryKey = `chat:${ChatId}`;
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useChatQuery({ queryKey, apiUrl, paramKey, paramValue });
   return (
     <>
       <div className="flex-1 flex flex-col py-4 overflow-y-auto">
         <div className="flex-1">
-          test
           <ChatWelcome type={type} name={name} />
         </div>
       </div>
