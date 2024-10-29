bc43bd53be263021e7e79de647048fd040d9235d
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-10-29 21:01:40 +0530

    changes

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,6 +1,11 @@
+import currentProfile from "@/lib/currentProfile";
+import { NextResponse } from "next/server";
+
 export async function GET(req: Request) {
   try {
+    const profile = await currentProfile();
   } catch (error) {
-    console.log("[SERVER_POST]", error);
+    console.log("[MESSAGES_GET]", error);
+    return new NextResponse("Internet Error", { status: 500 });
   }
 }
