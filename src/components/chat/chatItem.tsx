62ee207ac136ce10b3b8a1dec02fc29ed453fbaa
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-11-01 01:26:32 +0530

    added edit and delete button

--- src/components/chat/chatItem.tsx
+++ src/components/chat/chatItem.tsx
@@ -1,8 +1,13 @@
 import { Members, Profile } from "@prisma/client";
 import UserAvatar from "../ui/UserAvatar";
 import ActionTooltip from "../actionTooltip";
-import { FileIcon, ShieldAlert, ShieldCheck } from "lucide-react";
+import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface ChatItemProps {
   id: string;
@@ -23,6 +28,11 @@ const roleIconMap: any = {
   MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
   ADMIN: <ShieldAlert className="h-4 w-4 text-rose-500" />,
 };
+
+const formSchema = z.object({
+  content: z.string().min(1),
+});
+
 export default function ChatItem({
   id,
   content,
@@ -35,13 +45,29 @@ export default function ChatItem({
   socketUrl,
   socketQuery,
 }: ChatItemProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isDeleting, setisDeleting] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: content,
+    },
+  });
+
+  useEffect(() => {
+    form.reset({
+      content: content,
+    });
+  }, [form, content]);
+
   const fileTypes = fileUrl?.split(".").pop();
 
   const isAdmin = currentMember.role === "ADMIN";
   const isModerator = currentMember.role === "MODERATOR";
   const isOwner = currentMember.id === member.id;
-  const canDeleteMessages = !deleted && (isAdmin || isModerator || isOwner);
-  const canEditMessages = !deleted && isOwner && !fileUrl;
+  const canDeleteMessage = !deleted && (isAdmin || isModerator || isOwner);
+  const canEditMessage = !deleted && isOwner && !fileUrl;
   const isPDF = fileTypes === "pdf" && fileUrl;
   const isImage = !isPDF && fileUrl;
 
@@ -95,8 +121,37 @@ export default function ChatItem({
                 </a>
               </div>
             )}
+            {!fileUrl && !isEditing && (
+              <p
+                className={cn(
+                  "text-sm text-zinc-600 dark:text-zinc-300",
+                  deleted &&
+                    "italic text-zinc-500 dark:text-zinc-400 text-xs mt-1"
+                )}
+              >
+                {content}
+                {isUpdated && !deleted && (
+                  <span className="text-[10px] mx-2 text-zinc-500 dark:text-zinc-400">
+                    (edited)
+                  </span>
+                )}
+              </p>
+            )}
           </div>
         </div>
+
+        {canDeleteMessage && (
+          <div className="hidden group-hover:flex items-center gap-x-2 absolute p-1 -top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm">
+            {canEditMessage && (
+              <ActionTooltip label="Edit">
+                <Edit className="cursor-pointer ml-auto w-4 h-4text-zinc-500 hover:text-zinc-600 dark:hover: text-zinc-300 transition" />
+              </ActionTooltip>
+            )}
+            <ActionTooltip label="delte">
+              <Trash className="cursor-pointer ml-auto w-4 h-4text-zinc-500 hover:text-zinc-600 dark:hover: text-zinc-300 transition" />
+            </ActionTooltip>
+          </div>
+        )}
       </div>
     </>
   );
