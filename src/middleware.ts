d3cdb7e4e19f9dad29715c7862c349f92516096d
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-11-06 23:46:12 +0530

    remove coment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,3 @@
-// import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// // This example protects all routes including api/trpc routes
-// // Please edit this to allow other routes to be public as needed.
-// // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-// export default clerkMiddleware({});
-
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/api/uploadthing)"]);
